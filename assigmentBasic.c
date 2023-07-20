0c0c5498e340c4c3737519262e2ed25605f8c4ad
Aakar <aakaar.bh@gmail.com>
2023-07-20 23:21:14 +0530

    basic codes of C language

---
+++ assigmentBasic.c
@@ -0,0 +1,100 @@
+/*#include<stdio.h>
+int main()
+{
+	char al;
+	printf("enter alphabet : ");
+	scanf("%c",&al);
+	
+	if(al=='a' || al=='e' || al=='i' || al=='i' || al=='u')
+	{
+		printf("vovel");
+	}
+	else
+	{
+		printf("consonent");
+	}
+	return 0;
+}
+*/
+
+
+
+//leap year
+/*#include<stdio.h>
+int main()
+{
+	int year;
+	printf("enter year : ");
+	scanf("%d",&year);
+	
+	if(year%4==0 && (year%400==0 || year%100!=0))
+	{
+		printf("leap year");
+	}
+	else
+	{
+		printf("not a leap year");
+	}
+	return 0;
+}*/
+
+
+//match 
+/*#include<stdio.h>
+int main()
+{
+	char a;
+	printf("enter alphabet : ");
+	scanf("%c",&a);
+	
+	if(a=='a' || a=='f'||a=='g'||a=='h')
+	{
+		printf("its a match");
+	}
+	else
+	{
+		printf("not match");
+	}
+	return 0;
+}*/
+
+
+//male female
+/*#include<stdio.h>
+int main()
+{
+	char g;
+	printf("enter gender : ");
+	scanf("%c",&g);
+	
+	if(g=='f')
+	{
+		printf("female");
+	}
+	else
+	{
+		printf("male");
+	}
+	return 0;
+}*/
+
+//average
+//#include<stdio.h>
+//int main()
+//{
+//	int h,e,m,s,c,avg;
+//	printf("enter marks : ");
+//	scanf("%d\n%d\n%d\n%d\n%d",&h,&e,&m,&s,&c);
+//	avg=(h+e+m+s+c)/5;
+//
+//	if((h>=40&&e>=40&&m>=40&&s>=40&&c>=40)&&(avg>=40))
+//	{
+//		printf("pass with : %d",avg);
+//	}
+//	else
+//	{
+//		printf("fail");
+//	}
+//	return 0;
+//}
+
