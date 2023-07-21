4255204982d691943dec145a5a4fa83acc721b41
Aakar <aakaar.bh@gmail.com>
2023-07-21 12:21:17 +0530

    if else basic codes of c

---
+++ ifelse.c
@@ -0,0 +1,111 @@
+//if else if else
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter values ");
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a>b && a>c)
+	{
+		printf("a greater");
+	}
+	else if(b>a && b>c)
+	{
+		printf("b is greater");
+	}
+	else if(c>a && c>b)
+	{
+		printf("c is greater");
+	}
+	else
+	{
+		printf("all values are equal");
+	}
+	return 0;
+}*/
+
+
+/*#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e;
+	printf("enter values");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	
+	if(a>b&&a>c&&a>d&&a>e)
+	{
+		printf("a is max");
+	}
+	else if(b>a&&b>c&&b>d&&b>e)
+	{
+		printf("b is max");
+	}
+	else if(c>a && c>b && c>d && c>e)
+	{
+		printf("c is max");
+	}
+	else if(d>a && d>b && d>c && d>e)
+	{
+		printf("d is max");
+	}
+	else if(e>a && e>b && e>c && e>d)
+	{
+		printf("c is max");
+	}
+	else
+	{
+		printf("all are equal");
+	}
+	return 0;
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter values :");
+	scanf("%d",&a);
+	
+	if(a==1)
+	{
+		printf("sunday");
+	}
+    else if(a==2)
+	{
+		printf("monday");
+	}
+	else if(a==3)
+	{
+		printf("tuesday");
+	}
+	else if(a==4)
+	{
+		printf("wednessday");
+	}
+	else if(a==5)
+	{
+		printf("thursday");
+	}
+	else if(a==6)
+	{
+		printf("friday");
+	}
+	else if(a==7)
+	{
+		printf("monday");
+	}
+	else
+	{
+		printf("this is not week");
+	}
+	return 0;
+}*/
+
+
+// m M male  f F female Other
+
+
+
+
