f7466a063baa8083eb3dbf23fd6e70a1683e43fb
Aakar <aakaar.bh@gmail.com>
2023-07-26 17:19:41 +0530

    add funtions with User Input

---
+++ function_add2.c
@@ -0,0 +1,92 @@
+//1)no return and no argument two variables : int a , int b add() multi div modular sub with user input 
+//#include<stdio.h>
+//void add();
+//
+//void main()
+//{
+//	add();
+//}
+//
+//void add()
+//{
+//	int a,b;
+//	printf("enter both values : ");
+//	scanf("%d%d",&a,&b);
+//	int sum=a+b;
+//	printf("sum =%d",sum);
+//}
+
+
+
+
+//2)no return and argument two variables : int a , int b add() multi div modular sub with user input 
+
+//#include<stdio.h>
+//void add(int,int);
+//
+//void main(){
+//	int a,b;
+//	printf("enter both values : ");
+//	scanf("%d%d",&a,&b);
+//	add(a,b);
+//}
+//
+//void add(int a,int b)
+//{
+//	int sum=a+b;
+//	printf("sum = %d",sum);
+//}
+
+
+
+
+
+//3)return and no argument two variables : int a , int b add() multi div modular sub with user input 
+//#include<stdio.h>
+//int add();
+//
+//int main()
+//{
+//	int sum=add();
+//	printf("sum =%d",sum);
+//}
+//
+//int add()
+//{
+//	int a,b;
+//	printf("enter both values a & b: ");
+//	scanf("%d%d",&a,&b);
+//	int result=a+b;
+//	return result;
+//}
+
+
+
+
+
+
+
+
+//4)return and argument two variables : int a , int b add() multi div modular sub with user input 
+//#include<stdio.h>
+//int add(int,int);
+//
+//void main(){
+//	int a,b;
+//	printf("enter both a & b values : ");
+//	scanf("%d%d",&a,&b);
+//	int sum=add(a,b);
+//	printf("sum = %d",sum);
+//}
+//int add(int a,int b)
+//{
+//	int result=a+b;
+//	return result;
+//}
+
+
+
+
+
+
+
