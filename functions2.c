bd924f58c55da16436a3bd6a9639f0cb90755ed2
Aakar <aakaar.bh@gmail.com>
2023-07-26 14:31:58 +0530

    function with user input(argument/ no argument &no return

---
+++ functions2.c
@@ -0,0 +1,142 @@
+////with User Input
+
+//no return no argument
+//1. add a+b with user input
+//#include<stdio.h>
+//
+//void add();
+//
+//void main()
+//{
+//	add();
+//}
+//
+//void add(){
+//	int a,b;
+//	printf("enter both a & b values :");
+//	scanf(" %d %d",&a,&b);
+//	int r=a+b;
+//	printf("%d",r);
+//}
+
+//2. no return and argument(add int a ,float b) 
+//#include<stdio.h>
+//void add(int,float);
+//
+//void main()
+//{
+//	int a;
+//	float b;
+//	printf("enter both a & b values :");
+//	scanf("%d%f",&a,&b);
+//	add(a,b);
+//}
+//
+//void add(int a,float b)
+//{
+//	float sum=a+b;
+//	printf("%d+%f = %f",a,b,sum);	
+//}
+
+
+//3. no return and no arg add() int ,float
+//#include<stdio.h>
+//void add();
+//
+//void main()
+//{
+//	add();
+//}
+//
+//void add()
+//{
+//	int a;
+//	float b,sum;
+//	printf("enter both a and b values :");
+//	scanf("%d%f",&a,&b);
+//	sum=a+b;
+//	printf("sum = %f",sum);
+//	
+//}
+
+
+//4.1 no return and argument multiply int ,float
+//#include<stdio.h>
+//void multi(int,float);
+//
+//void main()
+//{
+//	int a;
+//	float b;
+//	printf("enter both a and b values :");
+//	scanf("%d%f",&a,&b);
+//	multi(a,b);
+//} 
+//
+//void multi(int a,float b)
+//{
+//	float result;
+//	result=a*b;
+//	printf("product = %f",result);
+//}
+
+
+//4.2 no return and argument divide int, float 
+
+//#include<stdio.h>
+//void div(int,float);
+//
+//void main()
+//{
+//	int a;
+//	float b;
+//	printf("enter both a and b values :");
+//	scanf("%d%f",&a,&b);
+//	div(a,b);
+//}
+//
+//void div(int x,float y)
+//{
+//	float result=x/y;
+//	printf("relsult = %f",result);
+//}
+
+
+//5.1 no argument and no return multiply int, float
+
+//#include<stdio.h>
+//void multi();
+//
+//void main(){
+//	multi();
+//}
+//
+//void multi()
+//{
+//	int a;
+//	float b;
+//	printf("enter both values : ");
+//	scanf("%d%f",&a,&b);
+//	float pro=a*b;
+//	printf("product = %f",pro);
+//}
+
+
+//5.2 argument and no return divide int, float
+
+//#include<stdio.h>
+//void div(int,float);
+//
+//void main(){
+//	int a;
+//	float b;
+//	printf("enter both a & b value:");
+//	scanf("%d%f",&a,&b);
+//	div(a,b);
+//}
+//
+//void div(int a,float b)
+//{
+//	float result=a/b;
+//	printf("result = %f",result);
+//}
