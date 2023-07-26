067ef089b38e6b24ea386561999871702d6cdc7e
Aakar <aakaar.bh@gmail.com>
2023-07-26 17:16:40 +0530

    funtions without userinput

---
+++ function_add1.c
@@ -0,0 +1,76 @@
+//1)no return and no argument two variables : int a , int b add() multi div modular sub without user input  
+
+//#include<stdio.h>
+//void add();
+//
+//void main()
+//{
+//	add();
+//}
+//
+//void add(){
+//	int a=2,b=5;
+//	int sum=a+b;
+//	printf("sum =%d",sum);
+//}
+
+
+
+//2)no return and argument two variables : int a , int b add() multi div modular sub without user input 
+
+//#include<stdio.h>
+//void add(int,int);
+//
+//void main()
+//{
+//	add(3,8);
+//}
+//
+//void add(int a,int b)
+//{
+//	int sum=a+b;
+//	printf("sum= %d",sum);
+//}
+
+
+
+//3)return and no argument two variables : int a , int b add() multi div modular sub without user input 
+
+//#include<stdio.h>
+//int add();
+//
+//void main()
+//{
+//	add();
+//	int sum=add();
+//	printf("sum= %d",sum);
+//}
+//
+//int add()
+//{
+//	int a=5,b=6,result=a+b;
+//	return result;
+//}
+
+
+
+
+//4)return and argument two variables : int a , int b add() multi div modular sub without user input  
+
+//#include<stdio.h>
+//int add(int,int);
+//
+//void main()
+//{
+//	int sum=add(3,6);
+//	printf("sum = %d",sum);
+//}
+//
+//int add(int a,int b){
+//	int result=a+b;
+//	return result;
+//		
+//}
+
+
+
