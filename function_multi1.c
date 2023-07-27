9e85d30eb3d9de7bfda7b51e6d3c8ad4246d1a6d
Aakar <aakaar.bh@gmail.com>
2023-07-27 12:55:22 +0530

    multiply functions with user input

---
+++ function_multi1.c
@@ -0,0 +1,81 @@
+
+
+
+//1)no return and no argument two variables : int a , int b  multi with user input 
+//#include<stdio.h>
+//void multi();
+//
+//void main()
+//{
+//	multi();
+//}
+//
+//void multi()
+//{
+//	int a,b;
+//	printf("enter both a and b values : ");
+//	scanf("%d%d",&a,&b);
+//	int result=a*b;
+//	printf("product =%d",result);
+//	
+//}
+
+
+
+
+//2)no return and argument two variables : int a , int b multi  sub with user input
+//#include<stdio.h>
+//void multi(int,int);
+//
+//int main()
+//{
+//	int a,b;
+//	printf("enter two no. :");
+//	scanf("%d%d",&a,&b);
+//	multi(a,b);
+//}
+//
+//void multi(int a,int b){
+//	int pro=a*b;
+//	printf("product= %d",pro);
+//}
+
+
+
+
+//3)return and no argument two variables : int a , int b  multi sub with user input
+//#include<stdio.h>
+//int multi();
+//
+//void main(){
+//	int result=multi();
+//	printf("product = %d",result);
+//}
+//
+//int multi(){
+//	int a,b;
+//	printf("enter both no. a & b :");
+//	scanf("%d%d",&a,&b);
+//	int result=a*b;
+//	return result;
+//}
+
+
+
+
+//4)return and argument two variables : int a , int b multi with user input
+//#include<stdio.h>
+//int multi(int,int);
+//
+//void main(){
+//	int a,b;
+//	printf("enter both a & b values : ");
+//	scanf("%d%d",&a,&b);
+//	int result = multi(a,b);
+//	printf("product =%d",result);
+//}
+//
+//int multi(int a,int b){
+//	int result=a*b;
+//	return result;
+//}
