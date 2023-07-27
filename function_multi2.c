24d8bcdf64d738310e662f9dbc8292b8fd32b7e6
Aakar <aakaar.bh@gmail.com>
2023-07-27 12:56:55 +0530

    multiply functions without user input

---
+++ function_multi2.c
@@ -0,0 +1,78 @@
+//1)no return and no argument two variables : int a , int b  multi without user input 
+//
+//#include<stdio.h>
+//
+//void multi();
+//
+//void main()
+//{
+//	multi();
+//}
+//
+//void multi()
+//{
+//	int a=3,b=5;
+//	int result=a*b;
+//	printf("result =%d",result);
+//}
+
+
+
+
+
+//2)no return and argument two variables : int a , int b  multi without user input
+//#include<stdio.h>
+//void multi(int ,int);
+//
+//void main()
+//{
+//	multi(4,10);
+//}
+//
+//void multi(int a,int b)
+//{
+//	int result=a*b;
+//	printf("product =%d",result);
+//}
+
+
+
+
+
+//3)return and no argument two variables : int a , int b  multi sub without user input 
+//#include<stdio.h>
+//
+//int multi();
+//
+//void main(){
+//	int result=multi();
+//	printf("product=%d",result);
+//}
+//
+//int multi(){
+//	int a=20,b=12;
+//	int pro=a*b;
+//	return pro;
+//}
+
+
+
+
+//4)return and argument two variables : int a , int b multi without user input
+//#include<stdio.h>
+//int multi(int,int);
+//
+//void main(){
+//	int a=4,b=16;
+//	int result=multi(a,b);
+//	printf("product = %d",result);
+//}
+//
+//int multi(int a,int b){
+//	int result= a*b;
+//	return result;
+//}
+
+
+
+
