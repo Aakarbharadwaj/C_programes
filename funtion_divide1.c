eb9b3203a612d3637ec0e26e6b467f1917dff350
Aakar <aakaar.bh@gmail.com>
2023-07-27 14:02:19 +0530

    divide funtions with user input

---
+++ funtion_divide1.c
@@ -0,0 +1,80 @@
+
+//1)no return and no argument two variables : int a , int b div with user input 
+//#include<stdio.h>
+//void div();
+//
+//void main(){
+//	div();
+//}
+//
+//void div(){
+//	int a,b;
+//	printf("enter both a and b valus :");
+//	scanf("%d%d",&a,&b);
+//	int result=a/b;
+//	printf("result =%d",result);
+//}
+
+
+
+
+
+//2)no return and argument two variables : int a , int b div with user input
+//#include<stdio.h>
+//void main(){
+//	int a,b;
+//	printf("enter both values a & b :");
+//	scanf("%d%d",&a,&b);
+//	div(a,b);
+//}
+//
+//void div(int a,int b){
+//	int result=a/b;
+//	printf("result =%d",result);
+//}
+
+
+
+
+
+//3)return and no argument two variables : int a , int b  divide with user input 
+//#include<stdio.h>
+//
+//int div();
+//
+//void main()
+//{
+//	int result=div();
+//	printf("result =%d",result);
+//}
+//
+//int div(){
+//	int a,b;
+//	printf("enter two values a & b: ");
+//	scanf("%d%d",&a,&b);
+//	int result =a/b;
+//	return result;
+//}
+
+
+
+
+
+
+//4)return and argument two variables : int a , int b div with user input 
+//#include<stdio.h>
+//void main(){
+//	int a,b;
+//	printf("enter both a & b values :");
+//	scanf("%d%d",&a,&b);
+//	int result=div(a,b);
+//	printf("result =%d",result);
+//}
+//
+//void div(int a,int b){
+//	int result=a/b;
+//	return result;
+//}
+
+
+
